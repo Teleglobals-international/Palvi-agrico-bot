c30bf82c57399521b1f4ab939e10d717e6d16d5a
Sagar Lad <sagar.lad@teleglobals.com>
2026-08-04 11:35:33 +0530

    Update CORS: restrict to localhost:3000

--- src/main.py
+++ src/main.py
@@ -58,7 +58,7 @@ app = FastAPI(title="Palvi Agrico Voice Bot", version="2.0.0", lifespan=lifespan
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
